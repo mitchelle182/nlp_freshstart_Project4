fc55d07aeccd05b10a3b8cfcfe502c1157183d09
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-05 20:16:29 -0400

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,21 +1,33 @@
 import { checkForUrl } from "./urlChecker";
 
 
+
 document.getElementById('submit').addEventListener('click', handleSubmit);
 
 async function handleSubmit(event) {
     event.preventDefault();
     // // check what text was put into the form field
+    console.log(input.value);
     let inputURL = document.getElementById('url').value;
     checkForUrl(inputURL);
 
     console.log("::: Form Submitted :::");
-    fetch('http://localhost:8000/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message;
-       
+    const response =  await fetch('http://localhost:8000/', {
+        method: 'POST', 
+        credentials: 'same-origin', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
     });
+    try {
+        const newData = await response.json();
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+        
+    
 }
 
 export { handleSubmit };
