676e24fd986ffc1a8b17da76be447bf5c9b8080a
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-08 23:39:16 -0400

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,6 +1,10 @@
+import { mcloudAPI } from "../../server/mcloudAPI";
 import { checkForUrl } from "./urlChecker";
 
 
+
+const input = document.querySelector("input");
+const form = document.querySelector("form");
 
 document.getElementById('submit').addEventListener('click', handleSubmit);
 
@@ -8,27 +12,18 @@ async function handleSubmit(event) {
     event.preventDefault();
     // // check what text was put into the form field
     console.log(input.value);
-    let inputURL = document.getElementById('url').value;
+    let inputURL = document.getElementById('url').value;  
     checkForUrl(inputURL);
 
     console.log("::: Form Submitted :::");
-    const response =  await fetch('http://localhost:8000/', {
-        method: 'POST', 
-        credentials: 'same-origin', 
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
-    try {
-        const newData = await response.json();
-        return newData;
-      }catch(error) {
-      console.log("error", error);
-      }
-        
+    mcloudAPI(url);
+    
     
 }
+
+        
+    
+
 
 export { handleSubmit };
 
