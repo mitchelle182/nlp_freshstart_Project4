346174e7c20bcc5ce322a0644acea5d3c20a4f9b
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-09 22:34:13 -0400

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const key = process.env.API_KEY;
+const key = process.env.API_KEY; 
 const express = require('express');
 const mcloudAPIResponse = require('./mcloudAPI.js');
 
 const app =  express();
 app.use(express.json());
-
-
-
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,16 +22,16 @@ console.log(__dirname);
 
 
 app.get('/', function (req, res) {
-//    res.send('src/server/index.js');
-    res.sendFile('dist/index.html');
+     res.send('src/server/index.js');
+    //res.sendFile('dist/index.html');
     // res.sendFile(path.resolve('src/client/views/index.html'))
 });
 
 app.post('/',  async (req, res) => {
-    const url = req.body.url;
-    const response = await mcloudAPIResponse.mcloudAPI(url, key);
-    res.send(response);
-   
+    //const url = req.body.url;
+    const response = await mcloudAPIResponse.mcloudAPI(url, key); //call API
+    res.send(response); //send back data to frontend
+   console.log(response);
 });
 
 
