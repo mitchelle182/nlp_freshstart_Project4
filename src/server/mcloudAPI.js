81cc704d131963e67752034a1c395cbca8c39190
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-09 22:33:32 -0400

    Add files via upload

--- src/server/mcloudAPI.js
+++ src/server/mcloudAPI.js
@@ -1,20 +1,19 @@
-const { response } = require("express");
-
 // from https://learn.meaningcloud.com/developer/sentiment-analysis/2.1/dev-tools 
-baseURL = "https://api.meaningcloud.com/sentiment-2.1";
- 
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
 
-const mcloudAPI = async (baseURL, key) => {
+
+const mcloudAPI = async (url, key) => {
   const response= await fetch(`${baseURL}?key${key}&lang=en&url=${url}`);
+ 
   try {
-    const data =await response.json();
+    const data = await response.json();
     console.log(data);
     return data;
 } catch(error){
     console.log("error", error);
 }
-
 };
+  
 
 
 
