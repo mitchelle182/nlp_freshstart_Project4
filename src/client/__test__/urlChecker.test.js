dd67824c5fd2f36e94dd73d49dfb85db6e290cc0
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-11 01:20:39 -0400

    Add files via upload

--- src/client/__test__/urlChecker.test.js
+++ src/client/__test__/urlChecker.test.js
@@ -1,3 +1,19 @@
-import { checkForUrl } from "../js/urlChecker";
+const { checkForUrl } = require("../js/urlChecker");
 
+describe('is it a url', () => {
+    test('test if strings are actual urls', () =>{
+        expect(checkForUrl("word")).toBeFalsy();
+    })
 
+    test('emails are not invalid urls', () => {
+        expect(checkForUrl("mailto:someone@email.com"))
+    })
+
+    test('valid urls to be true', () => {
+        expect(checkForUrl("https://www.youtube.com"))
+    })
+
+    test('empty strings are falsy', () => {
+        expect(checkForUrl("")).toBeFalsy();
+    })
+})
