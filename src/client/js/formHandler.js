62ebe4a5443e06446f7d40dbeec8afd7532e6ead
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-11 01:21:39 -0400

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,11 +1,4 @@
-
-import { mcloudAPI } from "../../server/mcloudAPI";
 import { checkForUrl } from "./urlChecker";
-
-
-
-const input = document.querySelector("input");
-
 
 
 document.getElementById('submit').addEventListener('click', handleSubmit);
@@ -13,24 +6,25 @@ document.getElementById('submit').addEventListener('click', handleSubmit);
 async function handleSubmit(event) {
     event.preventDefault();
     // // check what text was put into the form field
+    const input = document.querySelector("input");
     console.log(input.value);
     let inputURL = document.getElementById('url').value;
    
     checkForUrl(inputURL);
 
     console.log("::: Form Submitted :::");
-    mcloudAPI();
+   const response = await postData(inputURL);
     //postData('/', {text:data.text, score_tag:data.score_tag, subjectivity:data.subjectivity, agreement:data.agreement, confidence:data.confidence});
     //retrieveData();
+    const resultsNode = document.getElementById('results');
+    resultsNode.textContent = JSON.stringify(response, null, 2);
 }
 
 //send a POST Request to the backend to get back the data
-const postData = async ( url = '', data = {})=>{
-
-    const response = await fetch('/', {
+const postData = async (url)=>{
+    const data = {  url }
+    const response = await fetch('http://localhost:8000/', {
     method: 'POST', 
-    credentials: 'same-origin', 
-    redirect: 'follow',
     headers: {
         'Content-Type': 'application/json',
     },
@@ -48,23 +42,23 @@ const postData = async ( url = '', data = {})=>{
 
 
 //UI Update
-const retrieveData = async () => {
-    const request = await fetch('/');
-    try {
-        const allData =  await request.json();
-        console.log(allData);
+// const retrieveData = async () => {
+//     const request = await fetch('/');
+//     try {
+//         const allData =  await request.json();
+//         console.log(allData);
        
-        document.getElementById('text').innerHTML = allData.text;
-        document.getElementById('score_tag').innerHTML = allData.score_tag;
-        document.getElementById('subjectivity').innerHTML = allData.subjectivity;
-        document.getElementById('agreement').innerHTML = allData.agreement;
-        document.getElementById('confidence').innerHTML = allData.confidence;
-        document.getElementById('irony').innerHTML = allData.irony;
-    }
-    catch(error) {
-        console.log('error');
-    }
-};
+//         document.getElementById('text').innerHTML = allData.text;
+//         document.getElementById('score_tag').innerHTML = allData.score_tag;
+//         document.getElementById('subjectivity').innerHTML = allData.subjectivity;
+//         document.getElementById('agreement').innerHTML = allData.agreement;
+//         document.getElementById('confidence').innerHTML = allData.confidence;
+//         document.getElementById('irony').innerHTML = allData.irony;
+//     }
+//     catch(error) {
+//         console.log('error');
+//     }
+// };
         
     
 
--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -3,8 +3,7 @@ function checkForUrl(url) {
     if(/^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g.test(url)) {
          console.log('Valid');
      } else {
-         console.log('Invalid URL');
-         alert("Invalid URL. Please type in full web address.");
+         console.log('invalid');
      }
  }
   
