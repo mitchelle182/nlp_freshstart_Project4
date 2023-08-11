288d17366770ca3eeaa9941166d0fdf51f90ea9b
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-11 13:12:07 -0400

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,7 +1,7 @@
 import { checkForUrl } from "./urlChecker";
 
 
-document.getElementById('submit').addEventListener('click', handleSubmit);
+
 
 async function handleSubmit(event) {
     event.preventDefault();
@@ -14,15 +14,14 @@ async function handleSubmit(event) {
 
     console.log("::: Form Submitted :::");
    const response = await postData(inputURL);
-    //postData('/', {text:data.text, score_tag:data.score_tag, subjectivity:data.subjectivity, agreement:data.agreement, confidence:data.confidence});
-    //retrieveData();
+   
     const resultsNode = document.getElementById('results');
     resultsNode.textContent = JSON.stringify(response, null, 2);
 }
 
 //send a POST Request to the backend to get back the data
 const postData = async (url)=>{
-    const data = {  url }
+    const data = { url }
     const response = await fetch('http://localhost:8000/', {
     method: 'POST', 
     headers: {
@@ -41,26 +40,7 @@ const postData = async (url)=>{
 
 
 
-//UI Update
-// const retrieveData = async () => {
-//     const request = await fetch('/');
-//     try {
-//         const allData =  await request.json();
-//         console.log(allData);
-       
-//         document.getElementById('text').innerHTML = allData.text;
-//         document.getElementById('score_tag').innerHTML = allData.score_tag;
-//         document.getElementById('subjectivity').innerHTML = allData.subjectivity;
-//         document.getElementById('agreement').innerHTML = allData.agreement;
-//         document.getElementById('confidence').innerHTML = allData.confidence;
-//         document.getElementById('irony').innerHTML = allData.irony;
-//     }
-//     catch(error) {
-//         console.log('error');
-//     }
-// };
-        
-    
+
 
 
 export { handleSubmit };
